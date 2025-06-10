9d55a6b8e647737ae7bc1737e6e4f946b1bb52fb
Lucastaa <76867086+Lucastaa@users.noreply.github.com>
2025-06-10 09:12:32 +0700

    New formular

---
+++ App1/Lesson01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            Console.WriteLine("Enter the first number (a):");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the second number (b):");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            if (b == 0)
+            {
+                Console.WriteLine("Error: Division by zero is not allowed.");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Calculating a / b...");
+                c = a / b;
+                Console.WriteLine($"The result of {a} / {b} is: {c}");
+            }
+        }
+    }
+}
